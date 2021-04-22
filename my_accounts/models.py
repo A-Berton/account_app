ba563480cb0a7f92b2173431dcbe0db264a590f6
A.BERTON <bertonaubry.a@gmail.com>
2021-04-22 17:07:14 +0200

    adding models and migrations

--- my_accounts/models.py
+++ my_accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Account(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    balance = models.FloatField()
